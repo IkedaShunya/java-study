fdc7a69e048865db9e70da768ec66cb1ff1dc3a9
Ikeda Shunya <yukidaru.vov@gmail.com>
2024-09-23 16:06:59 +0900

    getとPost

--- src/main/java/raisetech/Student/Management/Application.java
+++ src/main/java/raisetech/Student/Management/Application.java
@@ -1,14 +1,22 @@
 package raisetech.Student.Management;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class Application {
+
+	//日本語を使うとエラーが発生する
+	private String name ="ikeda shunay";
+	private String age = "26";
+	//Map<String, Integer> student = new HashMap<String, Integer>();
 
 	public static void main(String[] args) {
 
@@ -16,10 +24,23 @@ public class Application {
 	}
 
 	//コミット用
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello, World";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo(){
+		return name + "　" + age +"歳";
 
+	}
+
+
+
+	//名前（URL）を分ける場合が多い
+	@PostMapping("/studentInfo")
+	public void setName(String name, String age){
+		this.name =name;
+		this.age = age;
+	}
+	@PostMapping("/studentName")
+	public void updateStudentName(String name){
+		this.name =name;
 	}
 
 
