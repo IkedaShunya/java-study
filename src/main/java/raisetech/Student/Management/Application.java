ee8e1d74f180e3b2b1dad2fdd78829128007332a
Ikeda Shunya <yukidaru.vov@gmail.com>
2024-09-29 19:25:38 +0900

    第16回23_MyBatisを使ってCRUDのRead処理を実装

--- src/main/java/raisetech/Student/Management/Application.java
+++ src/main/java/raisetech/Student/Management/Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -25,41 +23,17 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	//引数（名前）を指定してStudentRepositoryのselect文を実行
-	@GetMapping("/student")
-	//@RequestParamは（）内の変数のパラメータを指定できる　例）?name=ikedashunya
-	public String getStudentInfo(@RequestParam("name")String name){
-		Student student =repository.searchByName(name);
-		return student.getName() + student.getAge() + "歳";
 
-	}
-
-	//引数（名前と年齢）を指定してStudentRepositoryのINSERT文を実行
-	@PostMapping("/student")
-	public void registerStudent(String name, int age){
-		repository.registerStudent(name, age);
-	}
-
-	@PatchMapping("/student")
-	public void updateStudentAge(String name, int age){
-		repository.updateStudent(name,age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
-	}
-
-	//課題用
 	@GetMapping("/studentlist")
-	public List<Student> getStudentlist(){
-		List<Student> students = repository.searchByStudentlist();
-		return students;
+	public List<Student>  getStudentList(){
+		return repository.searchBystudent();
 
 	}
+	@GetMapping("/studentcourseslist")
+	public List<StudentsCourses>  getstudentcourseslist(){
+		return repository.searchBystudentCourese();
 
-
-
+	}
 
 
 }
--- src/main/java/raisetech/Student/Management/Student.java
+++ src/main/java/raisetech/Student/Management/Student.java
@@ -1,25 +1,20 @@
 package raisetech.Student.Management;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 //Mybatisが自動的にDBの情報をセットしてくれるいれもの
 public class Student {
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    //DBと同じ名前で紐づく
     private String name;
+    //キャメルケースのルールで大文字になる
+    private String nameRuby;
+    private String nickname;
+    private String emailAddress;
+    private String area;
     private int age;
-
+    private String gender;
 
 }
