6da625e30f6fc0f57d6eaa2334a0386ddd7db1f7
Ikeda Shunya <yukidaru.vov@gmail.com>
2024-10-11 07:08:56 +0900

    第34回 Create処理のREST化とリファクタリング

--- src/main/java/raisetech/Student/Management/repository/StudentRepository.java
+++ src/main/java/raisetech/Student/Management/repository/StudentRepository.java
@@ -13,30 +13,43 @@ import raisetech.Student.Management.data.StudentsCourses;
 
 
 /**
- * 受講生情報を扱うリポジトリ
- *
+ * 受講生情報と受講生コース情報を扱うリポジトリ
  * 全件、条件検索、コース検索が行えるクラス
  */
-//擬似的なデータベースみたいなもの
-//@MapperがつくとMyBatisが管理するということを自動的に認識してくれる
+
 @Mapper
 public interface StudentRepository {
     /**
      * 全件検索をします
+     *
+     * @return 受講生一覧（全件）
      */
     @Select("SELECT * FROM students WHERE delete_flag = 0 ORDER BY id")
     List<Student> searchBystudent();
+
+    /**
+     * 受講生のコース情報の全件検索を行います
+     * @return　受講生コース情報（全件）
+     */
     @Select("SELECT * FROM students_courses WHERE student_id IN (SELECT id FROM students WHERE delete_flag = 0) ORDER BY id")
     List<StudentsCourses> searchBystudentCourese();
 
     /**
-     * ID検索
+     * IDに紐づく受講生の検索を行います
+     * @param  id 受講生ID
+     * @return　受講生
      */
     @Select("SELECT * FROM students WHERE id IN (#{id}) ORDER BY id ")
     Student searchIdBystudent(int id);
 
+    /**
+     * IDに紐づく受講生コース情報の検索を行います
+     * @param  id 受講生ID
+     * @return　受講生コース情報
+     */
     @Select("SELECT * FROM students_courses WHERE student_id IN (#{studentid}) ORDER BY id")
     List<StudentsCourses>  searchCouresbystudentID(int id);
+
 
     @Insert("INSERT INTO students(name,name_ruby,nickname,email_address,area,age,gender,remark,delete_flag) " +
             "VALUES (#{name}, #{nameRuby}, #{nickname}, #{emailAddress}, #{area}, #{age}, #{gender} ,#{remark},0)")
