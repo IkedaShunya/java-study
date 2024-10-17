f92213f32c2c1da6d7a7cc06c669dd1c981c0a37
3030670 <3030670@30306-70>
2024-10-17 15:08:50 +0900

    完成！！！

--- src/main/java/raisetech/Student/Management/Controller/StudentController.java
+++ src/main/java/raisetech/Student/Management/Controller/StudentController.java
@@ -3,16 +3,19 @@ package raisetech.Student.Management.Controller;
 
 import java.util.List;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
 import raisetech.Student.Management.ExceptionHandler.TestException;
 import raisetech.Student.Management.domain.StudentDetail;
 import raisetech.Student.Management.service.StudentService;
@@ -39,11 +42,14 @@ public class StudentController {
         this.service = service;
 
     }
+    
+    @Operation(summary = "テスト用例外処理",description = "テスト用に例外処理をスローします")
     @GetMapping("/errtest")
     public void errTest() throws TestException{
         throw new TestException("現在このAPIは利用できません、");
     }
-
+    
+    
     /**
      * 受講生詳細一覧検索
      * 全件検索を行うので、条件指定は行わないものになります。
@@ -51,14 +57,14 @@ public class StudentController {
      * @return　受講生一覧（全件）
      */
 
-    @Operation(summary = "要約",description = "説明")
+    @Operation(summary = "受講生詳細一覧検索",description = "受講生詳細の一覧を検索します")
     @GetMapping("/studentlist")
     public List<StudentDetail> getStudentList() {
         return service.searchStudentList();
     }
 
 
-
+    
     /**
      * 受講生詳細の登録を行います
      *
@@ -67,7 +73,7 @@ public class StudentController {
      * @return　実行結果
      */
 
-    @Operation(summary = "受講生登録",description = "受講生を登録します" ,responses = {@ApiResponse(responseCode = "400")})
+    @Operation(summary = "受講生登録",description = "受講生を登録します")
     @PostMapping("/registerStudent")
     //StudentDetailオブジェクトにも＠Validを適用させる必要がある
     public ResponseEntity<StudentDetail> registerStudent(@RequestBody @Valid StudentDetail studentDetail){
@@ -75,7 +81,7 @@ public class StudentController {
     }
 
 
-
+    @Operation(summary = "受講生単一検索",description = "受講生IDから受講生の情報を検索します")
     @GetMapping("/student/{id}")
     //@PathVariableで{id}"を引数のString idに入れてくれる
     //1~3桁までしかいれれない
@@ -90,6 +96,7 @@ public class StudentController {
      * @param studentDetail　受講生小アイ
      * @return　実行結果
      */
+    @Operation(summary = "受講生情報更新",description = "受講生の詳細情報の検索を行います")
     @PutMapping("/updateStudent")
     //@ModelAttribute→@RequestBodyでRestか　 BindingResult resultは画面でのやつやから、消す
     public ResponseEntity<StudentDetail> updateStudent(@RequestBody @Valid StudentDetail studentDetail){
